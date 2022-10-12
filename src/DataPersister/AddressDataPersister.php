b7ce26c6b0a2384877845b777a87bc64bbdd7fad
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-10-12 23:05:35 +0200

    test request address php

---
+++ src/DataPersister/AddressDataPersister.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataPersister;
+
+use App\Entity\Address;
+use Doctrine\ORM\EntityManagerInterface;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+
+class AddressDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof Address;
+    }
+
+    public function persist($data, array $context = [])
+    {        
+        $this->em->persist($data);
+        $this->em->flush();
+    }
+
+    public function remove($data, array $context = [])
+    {
+        $this->em->remove($data);
+        $this->em->flush();
+    }
+}
