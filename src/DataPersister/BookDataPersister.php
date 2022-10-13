48a0cab7076104aa2bab279b1120ee98fc49ade9
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-10-14 00:57:50 +0200

    finish post and put

--- src/DataPersister/BookDataPersister.php
+++ src/DataPersister/BookDataPersister.php
@@ -21,20 +21,6 @@ class BookDataPersister implements ContextAwareDataPersisterInterface
 
     public function persist($data, array $context = [])
     {        
-
-/* $className = $this->em->getClassMetadata(get_class($object))->getName();
-
-
-
-        $author = $this->em->find($className, 100);
-        $book = new Book();
-        $book->addAuthor($author); */
-        
-//        $this->em->merge($author);
-
-
-
-
         $this->em->persist($data);
         $this->em->flush();
     }
