aab2acae17c4c633a2570dd756798023fa73356c
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-10-14 02:29:56 +0200

    create search by name for format

--- src/Entity/Format.php
+++ src/Entity/Format.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
-use ApiPlatform\Core\Bridge\Doctrine\MongoDbOdm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\FormatRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
