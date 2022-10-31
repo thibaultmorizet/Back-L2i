2e0311e93de773e2cfc50ed4c7bf81391e9430a7
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-10-31 01:25:07 +0100

    add var dump for test user data

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -24,7 +24,7 @@ class UserDataPersister implements ContextAwareDataPersisterInterface
 
     public function persist($data, array $context = [])
     {        
-        var_dump($data->getPassword());
+        var_dump($data->getPassword(),$context);
         if ($data->getPassword() /* && ($context['collection_operation_name'] ?? null) == "post" */) {
             $data->setPassword(
                 $this->passwordEncoder->hashPassword($data, $data->getPassword())
