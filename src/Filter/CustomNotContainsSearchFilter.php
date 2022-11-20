7ce3a556cedda60e6ffdc353040ad625e8232164
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-11-20 23:39:18 +0100

    add search filter on user roles

--- src/Filter/CustomNotContainsSearchFilter.php
+++ src/Filter/CustomNotContainsSearchFilter.php
@@ -28,6 +28,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 
 /**
  * Filter the collection by given properties.
