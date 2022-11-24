bd1a9d55769365b15b767bef13e765895c9c5b23
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-11-24 09:59:18 +0100

    start to set mailer

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/mail', name: 'email')]
+    public function sendMail(MailerInterface $mailer)
+    {
+        $mail = (new Email())
+            ->from('thibaultmorizet@icloud.com')
+            ->to('thibaultmorizet@icloud.com')
+            ->subject('Mon beau sujet')
+            ->html('<p>Ceci est mon message en HTML</p>');
+
+        $mailer->send($mail);
+    }
+}
