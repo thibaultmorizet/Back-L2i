841ceb3df01e82a0331609535f0427274f8f46c6
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-12-14 19:25:25 +0100

    add search by book id

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\MongoDbOdm\Filter\NumericFilter;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
