7cab0d6fd92e938ce1c566cfd389d5b35bfe5331
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-12-15 16:41:47 +0100

    update image to upload function

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -18,7 +18,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
     normalizationContext: ['groups' => "image:read"],
     denormalizationContext: ['groups' => "image:write"],
-    order: ['book.id' => 'ASC', 'position' => 'ASC']
+    order: ['position' => 'ASC']
 )]
 #[ApiFilter(
     NumericFilter::class,
