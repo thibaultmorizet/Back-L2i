d5f08d66713b51ae6e093ddfc63af31a953a66a9
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-12-28 12:05:06 +0100

    update comment relation with other table

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\NumericFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
