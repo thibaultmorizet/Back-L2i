c4736cb9f0d472afc5e6484bf71d59a7f0d28605
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-12-28 13:59:51 +0100

    update comment relation with other table

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -35,7 +35,7 @@ class Comment
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, options: ["default" => 'CURRENT_TIMESTAMP'], nullable: true)]
     #[Groups(["product:read", "comment:read", "user:read"])]
-    private ?\DateTimeInterface $createdAt;
+    private ?\DateTimeInterface $createdAt = date("Y-m-d H:i:s", time());
 
     #[ORM\ManyToOne(inversedBy: 'comments')]
     #[ORM\JoinColumn(nullable: false)]
