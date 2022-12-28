3f63e57b26160f9ca07c28aaa159f0af25a245ee
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-12-28 13:35:51 +0100

    update comment relation with other table

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -64,12 +64,12 @@ class Comment
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
