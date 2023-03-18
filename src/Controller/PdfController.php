b6050cbb536105c4bfb4251fe1d36d4bee65487a
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-18 18:11:35 +0100

    continue html2pdf integration

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -10,18 +10,20 @@ use Spipu\Html2Pdf\Html2Pdf;
 
 class PdfController extends AbstractController
 {
-    #[Route('/pdf/{id}', name: 'pdf')]
+    private $pdf;
+
+    #[Route('/pdf', name: 'pdf')]
     public function showPdf(Order $order): Response
     {
         return $template = $this->render('pdf/pdf.html.twig', [
             'order' => $order,
         ]);
 
-      //$html2pdf = $this->get('app.html2pdf');
       $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
 
-      return $html2pdf->generatePdf($template, "facture");
+     // return $this->generatePdf($template, "facture");
     }
-
+    
+    
 
 }
