aa3eb98118f791eb539a098db1374659ae8b5ef8
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-18 18:21:52 +0100

    update pdf route

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -2,20 +2,20 @@
 
 namespace App\Controller;
 
-use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Spipu\Html2Pdf\Html2Pdf;
+use Symfony\Component\HttpFoundation\Request;
 
 class PdfController extends AbstractController
 {
 
     #[Route('/pdf', name: 'pdf')]
-    public function showPdf(Order $order): Response
+    public function showPdf(Request $request): Response
     {
         return $template = $this->render('pdf/pdf.html.twig', [
-            'order' => $order,
+            'order' => $request,
         ]);
 
       $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
