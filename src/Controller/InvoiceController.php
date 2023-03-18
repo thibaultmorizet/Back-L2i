3e7bdb2adebc5dbf46af889e69d78afb151fef24
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-18 21:35:08 +0100

    change library for generate pdf

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Spipu\Html2Pdf\Html2Pdf;
 use Symfony\Component\HttpFoundation\Request;
+use Dompdf\Dompdf;
 
 class InvoiceController extends AbstractController
 {
@@ -22,14 +23,23 @@ class InvoiceController extends AbstractController
         $template = $this->render('invoice/invoice.twig.html', [
             'order' => $parametersAsArray,
         ]);
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($template);
+        $dompdf->render();
+         
+        return new Response (
+            $dompdf->stream('resume', ["Attachment" => false]),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/pdf']
+        );
 
-        $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
+        // $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
 
-        $html2pdf->writeHTML($template);
+        // $html2pdf->writeHTML($template);
 
 
 
-        $html2pdf->output();
+        // $html2pdf->output();
         return $this->json(
             [
                 "success" => true,
