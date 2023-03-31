138a92c1efddce39261636b99fdbd651702b4707
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-31 12:04:26 +0200

    update controller for test

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -14,37 +14,30 @@ class InvoiceController extends AbstractController
     #[Route('/generate_invoice', name: 'generateInvoice')]
     public function showInvoice(Request $request): Response
     {
-           return $this->json(
+                
+        $parametersAsArray = [];
+        if ($content = $request->getContent()) {
+            $parametersAsArray = json_decode($content, true);
+        }
+
+        $template = $this->render('invoice/invoice.twig.html', [
+            'order' => $parametersAsArray,
+        ]);
+   
+        $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
+
+        $html2pdf->writeHTML($template);
+
+
+// var_dump(getcwd());
+        $html2pdf->output( getcwd() . "/assets/invoice/". $parametersAsArray["id"] . ".pdf","F");
+
+        return $this->json(
             [
                 "success" => true,
                 "message" => "Invoice created"
             ],
             200,
-        );  
-        
-        // $parametersAsArray = [];
-        // if ($content = $request->getContent()) {
-        //     $parametersAsArray = json_decode($content, true);
-        // }
-
-        // $template = $this->render('invoice/invoice.twig.html', [
-        //     'order' => $parametersAsArray,
-        // ]);
-   
-        // $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
-
-        // $html2pdf->writeHTML($template);
-
-
-// var_dump(getcwd());
-        // $html2pdf->output( getcwd() . "/assets/invoice/". $parametersAsArray["id"] . ".pdf","F");
-
-        // return $this->json(
-        //     [
-        //         "success" => true,
-        //         "message" => "Invoice created"
-        //     ],
-        //     200,
-        // );
+        );
     }
 }
