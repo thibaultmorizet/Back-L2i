9a366ece678bf85e6b613f654266a463a6fe45e8
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-31 15:59:54 +0200

    add route and function isInvoiceExist

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Spipu\Html2Pdf\Html2Pdf;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Uid\UuidV6;
+use Symfony\Component\Filesystem\Filesystem;
 
 class InvoiceController extends AbstractController
 {
@@ -15,7 +17,7 @@ class InvoiceController extends AbstractController
     #[Route('/generate_invoice', name: 'generateInvoice')]
     public function showInvoice(Request $request): Response
     {
-                
+
         $parametersAsArray = [];
         if ($content = $request->getContent()) {
             $parametersAsArray = json_decode($content, true);
@@ -24,22 +26,40 @@ class InvoiceController extends AbstractController
         $template = $this->render('invoice/invoice.twig.html', [
             'order' => $parametersAsArray,
         ]);
-   
+
         $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
 
         $html2pdf->writeHTML($template);
 
         $uuid = new UuidV6();
 
-        $html2pdf->output( getcwd() . "/assets/invoice/". $uuid . ".pdf","F");
-       
+        $html2pdf->output(getcwd() . "/assets/invoice/" . $uuid . ".pdf", "F");
+
         return $this->json(
             [
                 "success" => true,
                 "message" => "Invoice created",
-                "invoice_path" => "https://thibaultmorizet.fr/assets/invoice/". $uuid . ".pdf"
+                "invoice_path" => "https://thibaultmorizet.fr/assets/invoice/" . $uuid . ".pdf"
             ],
             200,
         );
+    }
+
+    #[Route('/invoice_is_exist', name: 'invoiceIsExist')]
+    public function invoiceIsExist(Request $request): bool
+    {
+
+        $parametersAsArray = [];
+        if ($content = $request->getContent()) {
+            $parametersAsArray = json_decode($content, true);
+        }
+
+var_dump($parametersAsArray);
+
+        $fileSystem = new Filesystem();
+        // $fileSystem->exists();
+
+
+        return true;
     }
 }
