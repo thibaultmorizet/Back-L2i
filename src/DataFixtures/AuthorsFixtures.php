e1f4f3c58825835e58535e1c120d73e62b8e52c6
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-04-07 07:03:50 +0200

    update name

--- src/DataFixtures/AuthorsFixtures.php
+++ src/DataFixtures/AuthorsFixtures.php
@@ -10,47 +10,47 @@ class AuthorsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $auteur1 = new Author();
-        $auteur1->setLastname("Rimelé");
-        $auteur1->setFirstname("Rodolphe");
-        $auteur1->setLanguage('FR');
-        $manager->persist($auteur1);
-        $this->addReference('auteur1', $auteur1);
+        $author1 = new Author();
+        $author1->setLastname("Rimelé");
+        $author1->setFirstname("Rodolphe");
+        $author1->setLanguage('FR');
+        $manager->persist($author1);
+        $this->addReference('author1', $author1);
 
-        $auteur2 = new Author();
-        $auteur2->setLastname("Rollet");
-        $auteur2->setFirstname("Olivier");
-        $auteur2->setLanguage('FR');
-        $manager->persist($auteur2);
-        $this->addReference('auteur2', $auteur2);
+        $author2 = new Author();
+        $author2->setLastname("Rollet");
+        $author2->setFirstname("Olivier");
+        $author2->setLanguage('FR');
+        $manager->persist($author2);
+        $this->addReference('author2', $author2);
 
-        $auteur3 = new Author();
-        $auteur3->setLastname("Martin");
-        $auteur3->setFirstname("Robert C.");
-        $auteur3->setLanguage('EN');
-        $manager->persist($auteur3);
-        $this->addReference('auteur3', $auteur3);
+        $author3 = new Author();
+        $author3->setLastname("Martin");
+        $author3->setFirstname("Robert C.");
+        $author3->setLanguage('EN');
+        $manager->persist($author3);
+        $this->addReference('author3', $author3);
 
-        $auteur4 = new Author();
-        $auteur4->setLastname("Valade");
-        $auteur4->setFirstname("Janet");
-        $auteur4->setLanguage('FR');
-        $manager->persist($auteur4);
-        $this->addReference('auteur4', $auteur4);
+        $author4 = new Author();
+        $author4->setLastname("Valade");
+        $author4->setFirstname("Janet");
+        $author4->setLanguage('FR');
+        $manager->persist($author4);
+        $this->addReference('author4', $author4);
 
-        $auteur5 = new Author();
-        $auteur5->setLastname("Anquetil");
-        $auteur5->setFirstname("Roxane");
-        $auteur5->setLanguage('FR');
-        $manager->persist($auteur5);
-        $this->addReference('auteur5', $auteur5);
+        $author5 = new Author();
+        $author5->setLastname("Anquetil");
+        $author5->setFirstname("Roxane");
+        $author5->setLanguage('FR');
+        $manager->persist($author5);
+        $this->addReference('author5', $author5);
 
-        $auteur6 = new Author();
-        $auteur6->setLastname("Dordoigne");
-        $auteur6->setFirstname("José");
-        $auteur6->setLanguage('FR');
-        $manager->persist($auteur6);
-        $this->addReference('auteur6', $auteur6);
+        $author6 = new Author();
+        $author6->setLastname("Dordoigne");
+        $author6->setFirstname("José");
+        $author6->setLanguage('FR');
+        $manager->persist($author6);
+        $this->addReference('author6', $author6);
 
         $manager->flush();
     }
