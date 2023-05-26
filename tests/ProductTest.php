ec0235be7f84c4820f59192dd5dd1662285f7604
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-05-26 08:52:50 +0200

    start to add test in backend

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -10,17 +10,18 @@ class ProductTest extends ApiTestCase
     public function testGetCollection(): void
     {
 
-        $client = static::createClient();
+        $client = static::createClient(
+            defaultOptions: [
+                'headers' => ['accept' => ['application/json']],
+                'base_uri' => 'http://127.0.0.1:8000'
+            ]
+        );
         $client->request('GET', '/ws/products');
 //        var_dump($client);
 
 
 //        $this->assertResponseStatusCodeSame(406);
         $this->assertResponseIsSuccessful();
-
-
-
-
 
 
     }
