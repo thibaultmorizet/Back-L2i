8f902e6eaedfc6273bf7befae98d7c765929db63
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-06-23 10:31:27 +0200

    update fixture addresses

--- src/DataFixtures/AddressFixtures.php
+++ src/DataFixtures/AddressFixtures.php
@@ -20,8 +20,13 @@ class AddressFixtures extends Fixture
         $address1->setCountry("France");
         $manager->persist($address1);
         $this->addReference('address1', $address1);
-        $manager->persist($address1);
-        $this->addReference('address2', $address1);
+
+        $address2 = new Address();
+        $address2->setStreet('Impasse des coquelicots');
+        $address2->setPostalcode(84000);
+        $address2->setCity("Avignon");
+        $address2->setCountry("France");$manager->persist($address2);
+        $this->addReference('address2', $address2);
 
         $address3 = new Address();
         $address3->setStreet($faker->streetName);
@@ -30,8 +35,14 @@ class AddressFixtures extends Fixture
         $address3->setCountry("France");
         $manager->persist($address3);
         $this->addReference('address3', $address3);
-        $manager->persist($address3);
-        $this->addReference('address4', $address3);
+
+        $address4 = new Address();
+        $address4->setStreet($address3->getStreet());
+        $address4->setPostalcode($address3->getPostalcode());
+        $address4->setCity($address3->getCity());
+        $address4->setCountry("France");
+        $manager->persist($address4);
+        $this->addReference('address4', $address4);
 
         $address5 = new Address();
         $address5->setStreet($faker->streetName);
@@ -40,8 +51,14 @@ class AddressFixtures extends Fixture
         $address5->setCountry("France");
         $manager->persist($address5);
         $this->addReference('address5', $address5);
-        $manager->persist($address5);
-        $this->addReference('address6', $address5);
+
+        $address6 = new Address();
+        $address6->setStreet($address5->getStreet());
+        $address6->setPostalcode($address5->getPostalcode());
+        $address6->setCity($address5->getCity());
+        $address6->setCountry("France");
+        $manager->persist($address6);
+        $this->addReference('address6', $address6);
 
         $manager->flush();
     }
